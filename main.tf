2d5f38aa0424376a25dbb89db5d422bd8605244e
sagar <dhawlesagar775@gmail.com>
2026-05-01 14:47:54 +0530

    fix ami error

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ provider "aws" {
 resource "aws_instance" "cicd" {
   instance_type = var.instance_type
   key_name = var.key_name
+  ami = data.aws_ami.amazon_linux.id
   
   tags = {
     Name = "CICD"
