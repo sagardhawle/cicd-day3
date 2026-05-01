2c9721b917172e211e45a39594c56a2b36f12664
sagar <dhawlesagar775@gmail.com>
2026-05-01 14:57:08 +0530

    add web server

--- main.tf
+++ main.tf
@@ -1,16 +1,41 @@
+
 provider "aws" {
   region = var.region
 }
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
 
 
 resource "aws_instance" "cicd" {
-  instance_type = var.instance_type
-  key_name = var.key_name
-  ami = data.aws_ami.amazon_linux.id
-  
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    apt update -y
+    apt install nginx -y
+    systemctl start nginx
+    echo "<h1>CI/CD Terraform Website 🚀</h1>" > /var/www/html/index.html
+  EOF
+
   tags = {
-    Name = "CICD"
+    Name = "CICD-Instance"
   }
 }
