0ad01a102271c91198865cfb056ff351f96b6bd7
sagar <dhawlesagar775@gmail.com>
2026-05-01 15:32:11 +0530

    fix user_data html rendering

--- main.tf
+++ main.tf
@@ -33,17 +33,49 @@ resource "aws_instance" "cicd" {
   key_name = var.key_name
 
   vpc_security_group_ids = [aws_security_group.web_sg.id]
+user_data = <<-EOF
+  #!/bin/bash
+  yum update -y
+  amazon-linux-extras install nginx1 -y
+  systemctl start nginx
+  systemctl enable nginx
 
-  user_data = <<-EOF
-    #!/bin/bash
-    yum update -y
-    amazon-linux-extras install nginx1 -y
-    systemctl start nginx
-    systemctl enable nginx
-    echo "<h1>CI/CD Terraform Website 🚀</h1>" > /usr/share/nginx/html/index.html
-  EOF
+  cat <<HTML > /usr/share/nginx/html/index.html
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <title>CI/CD Project</title>
+      <style>
+          body {
+              text-align: center;
+              font-family: Arial, sans-serif;
+              background-color: #0f172a;
+              color: white;
+          }
+          h1 {
+              margin-top: 50px;
+              font-size: 40px;
+          }
+          .logos img {
+              width: 120px;
+              margin: 20px;
+          }
+      </style>
+  </head>
+  <body>
 
-  tags = {
-    Name = "CICD-Instance"
-  }
+      <h1>Hey Sagar 🚀</h1>
+      <p>Deployed using Terraform + GitHub CI/CD + AWS</p>
+
+      <div class="logos">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/04/Terraform_Logo.svg">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/93/Amazon_Web_Services_Logo.svg">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg">
+      </div>
+
+  </body>
+  </html>
+  HTML
+EOF
 }
